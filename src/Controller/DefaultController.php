a3b3fd53f4032506e7e07f5262692844e05df4c5
pow3rr3d <adrien.messager.diaz@gmail.com>
2023-05-19 11:26:32 +0200

    add user crud in admin side

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,12 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-#[Route('/', name: 'homepage')]
+#[Route('/')]
 class DefaultController extends AbstractController{
 
-    #[Route('/')]
+    #[Route('/dashboard', name: 'dashboard_index')]
     public function index():Response
     {
-        return $this->render('index.html.twig');
+        $user = $this->getUser();
+       
+        return $this->render('index.html.twig', ["user" => $user]);
     }
 }
