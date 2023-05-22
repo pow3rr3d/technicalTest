767f3e4de42d824b260905263bb18d564bea05c2
pow3rr3d <adrien.messager.diaz@gmail.com>
2023-05-22 14:00:19 +0200

    add styles

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/account')]
+class AccountController extends AbstractController
+{
+    #[Route('/', name: 'app_user_account_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, UserRepository $userRepository): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+            $request->getSession()->replace(['_locale' => $this->getUser()->getLocal()->getCode() ]);
+
+            return $this->redirectToRoute('dashboard_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+
+}
